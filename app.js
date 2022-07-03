931a09d8647f9a707ecb1fc888122bb06ceeb345
MioRain <ericzero1593@hotmail.com>
2022-07-03 14:44:40 +0800

    feat: add facebook login functionality and remove fb secret key

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -7,13 +10,13 @@ const routes = require('./routes')
 const usePassport = require('./config/passport')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
