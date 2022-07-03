58c0a85f34c60c760b2e4ac10f4327554706e614
MioRain <ericzero1593@hotmail.com>
2022-07-03 13:15:40 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 
@@ -19,16 +20,19 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(flash())
 
 usePassport(app)
-
-app.use(routes)
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
 module.exports = {
   authenticator: (req, res, next) => {
     if (req.isAuthenticated()) {
+      console.log(req.isAuthenticated())
       return next()
     }
+    console.log(req.isAuthenticated())
+    req.flash('warning_msg', '請先登入才能使用！')
     res.redirect('/users/login')
   }
 }
